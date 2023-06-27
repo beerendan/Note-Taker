5a83ed5a8af39d9bccc8a4d0300f3905f45db32c
Beerendan <brendanrbowen@gmail.com>
2023-06-27 19:38:46 -0400

    added file structure for routes and their use in server.js

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express=require("express");
+const html=require("./routes/html")
+const api=require("./routes/api")
 
 //express server setup
 const app=express();
@@ -10,3 +12,7 @@ app.use(express.static("public"));
 //include middleware
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
+
+//configure routes
+app.use("/",html);
+app.use("/api",api)
